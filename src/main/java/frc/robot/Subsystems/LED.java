a96ca0f4e473168a704395e2f0471d3fd44f1355
Aidan <aidan.290769@nv.ccsd.net>
2023-03-28 14:27:59 -0700

    Moved led strobe to separate function

--- src/main/java/frc/robot/Subsystems/LED.java
+++ src/main/java/frc/robot/Subsystems/LED.java
@@ -4,24 +4,17 @@
 
 package frc.robot.Subsystems;
 
-import java.util.ArrayList;
-
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 
 public class LED extends SubsystemBase {
-  private static AddressableLED m_bottomLED;
-  private static AddressableLED m_topLeftLED;
-  private static AddressableLED m_topRightLED;
+  private static AddressableLED m_led;
 
-  private static AddressableLEDBuffer m_bottomLEDBuffer;
-  private static AddressableLEDBuffer m_topLeftLEDBuffer;
-  private static AddressableLEDBuffer m_topRightLEDBuffer;
+  private static AddressableLEDBuffer m_ledBuffer;
 
   private static Color m_red;
   private static Color m_gold;
@@ -36,13 +29,9 @@ public class LED extends SubsystemBase {
 
   /** Creates a new LED. */
   public LED() {
-    m_bottomLED = new AddressableLED(2);
-    //m_topLeftLED = new AddressableLED(1);
-    //m_topRightLED = new AddressableLED(0);
+    m_led = new AddressableLED(9);
 
-    m_bottomLEDBuffer = new AddressableLEDBuffer(43);
-    //m_topLeftLEDBuffer = new AddressableLEDBuffer(40);
-    //m_topRightLEDBuffer = new AddressableLEDBuffer(40);
+    m_ledBuffer = new AddressableLEDBuffer(123);
 
     m_red = new Color(240, 33, 0);
     m_gold = new Color(228, 211, 0);
@@ -55,50 +44,37 @@ public class LED extends SubsystemBase {
 
     m_timer.start();
 
-    m_bottomLED.setLength(m_bottomLEDBuffer.getLength());
-    //m_topLeftLED.setLength(m_topLeftLEDBuffer.getLength());
-    //m_topRightLED.setLength(m_topRightLEDBuffer.getLength());
+    m_led.setLength(m_ledBuffer.getLength());
 
-    m_bottomLED.setData(m_bottomLEDBuffer);
-    //m_topLeftLED.setData(m_topLeftLEDBuffer);
-    //m_topRightLED.setData(m_topRightLEDBuffer);
+    m_led.setData(m_ledBuffer);
 
-    m_bottomLED.start();
-    //m_topLeftLED.start();
-    //m_topRightLED.start();
-
-    for (int i = 0; i < 43; i++) {
-      if (i % 2 == 0) {
-        m_bottomLEDBuffer.setLED(i, m_yellow);
-      } else {
-        m_bottomLEDBuffer.setLED(i, m_red);
-      }
-    }
+    m_led.start();
   }
 
   @Override
   public void periodic() {
-    m_miliseconds = Units.secondsToMilliseconds(Math.floor(m_timer.get()));
-    System.out.println(m_miliseconds);
-    System.out.println(m_miliseconds);
+    m_miliseconds = Math.floor(Units.secondsToMilliseconds(m_timer.get()) / 100) * 100;
+  }
+
+  private void strobe() {
     if (m_miliseconds % 1000 == 0.0) {
       for (int i = 0; i < 43; i++) {
         if (i % 2 == 0) {
-          m_bottomLEDBuffer.setLED(i, m_yellow);
+          m_ledBuffer.setLED(i, m_yellow);
         } else {
-          m_bottomLEDBuffer.setLED(i, m_red);
+          m_ledBuffer.setLED(i, m_red);
         }
       }
     } else if (m_miliseconds % 500 == 0.0) {
       for (int i = 0; i < 43; i++) {
         if (i % 2 == 0) {
-          m_bottomLEDBuffer.setLED(i, m_red);
+          m_ledBuffer.setLED(i, m_red);
         } else {
-          m_bottomLEDBuffer.setLED(i, m_yellow);
+          m_ledBuffer.setLED(i, m_yellow);
         }
       }
     }
 
-    m_bottomLED.setData(m_bottomLEDBuffer);
+    m_led.setData(m_ledBuffer);
   }
 }
